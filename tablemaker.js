7065daceebeb9c6a644fb3c3595a19cff250acae
samoen <samoen@gmail.com>
2018-08-07 18:44:43 +0800

    can make tables from objects

--- tablemaker.js
+++ tablemaker.js
@@ -1,4 +1,4 @@
-function ShowTable(dataToShow,insertTableBefore){
+function ShowTableFromArray(dataToShow,insertTableBefore){
     var table = document.createElement("table");
     for(var i = 0; i < dataToShow.length; i++) {
         var tr = document.createElement("tr");
@@ -7,5 +7,22 @@ function ShowTable(dataToShow,insertTableBefore){
         tr.appendChild(td);
         table.appendChild(tr);
     }
+    document.body.insertBefore(table,document.getElementById(insertTableBefore))
+}
+
+function ShowTableFromObject(dataToShow,insertTableBefore){
+    var table = document.createElement("table");
+    table.border = 1;
+    Object.entries(dataToShow).forEach(
+    ([key, value]) => {
+        var tr = document.createElement("tr");
+        var td = document.createElement("td");
+        var td2 = document.createElement("td");
+        td.appendChild(document.createTextNode(value));
+        td2.appendChild(document.createTextNode(key));
+        tr.appendChild(td2);
+        tr.appendChild(td);
+        table.appendChild(tr);
+    });
     document.body.insertBefore(table,document.getElementById(insertTableBefore))
 }
