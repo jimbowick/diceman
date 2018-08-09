430ed7eb114b4f8c74f516f0ed2c4ce9ae288500
samoen <samoen@gmail.com>
2018-08-09 21:51:09 +0800

    button adds an item to the table

--- tablemaker.js
+++ tablemaker.js
@@ -12,6 +12,7 @@ function ShowTableFromArray(dataToShow,insertTableBefore){
 
 function ShowTableFromObject(dataToShow,insertTableBefore){
     var table = document.createElement("table");
+    table.id = "objecttable"
     table.border = 1;
     Object.entries(dataToShow).forEach(
     ([key, value]) => {
@@ -26,3 +27,47 @@ function ShowTableFromObject(dataToShow,insertTableBefore){
     });
     document.body.insertBefore(table,document.getElementById(insertTableBefore))
 }
+
+function ShowTableFromArrayOfObjects(dataToShow,insertTableBefore){
+    var table = document.createElement("table");
+    table.id = "movestable"
+    table.border = 1;
+    var htr = document.createElement("tr");
+    
+    Object.entries(dataToShow[0]).forEach(([key, value]) => {
+        var bold = document.createElement("strong");
+        var td = document.createElement("td");
+        td.appendChild(document.createTextNode(key));
+        bold.appendChild(td)
+        var anothertd = document.createElement("td");
+        anothertd.appendChild(bold);
+        htr.appendChild(anothertd);
+    });
+   
+    table.appendChild(htr)
+    for(var i = 0; i < dataToShow.length; i++) {
+        var tr = document.createElement("tr");
+        Object.entries(dataToShow[i]).forEach(([key, value]) => {        
+                var td = document.createElement("td");
+                td.appendChild(document.createTextNode(value));
+                tr.appendChild(td);
+        });
+        table.appendChild(tr);
+    }
+    document.body.insertBefore(table,document.getElementById(insertTableBefore))
+}
+var player = {health:"40", attack:"3", moveset:[{movename: "surestrike",dmg:"8"},{movename:"lightstrike",dmg:"4"}]}
+    var equipped = {weapon:"sword of ages", armor:"blessed breastplate"}
+    ShowTableFromArray(["hi","I like", "food","lots"],"simpletable")
+    ShowTableFromObject(equipped,"simpletable")
+    ShowTableFromArrayOfObjects(player.moveset,"spacer")
+    document.getElementById("wandbut").onclick = function(){ 
+        if( !player.moveset.find(mov=>mov.movename=="newmove") ){
+            player.moveset.push({movename:"newmove",dmg:"300"})
+            var elem = document.getElementById("movestable");
+            elem.parentNode.removeChild(elem);
+            ShowTableFromArrayOfObjects(player.moveset,"spacer")
+        }else{
+            document.getElementById("wandbut").innerHTML = "you already got your move!"
+        }
+     }
